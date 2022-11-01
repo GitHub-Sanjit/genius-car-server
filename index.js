13bcaa4390916ef10c3bd303c1c5a059bd333f8b
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-11-01 21:16:14 +0600

    67-1 Recap set login and set basic server01

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Server side");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleWares
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("genius car server is running");
+});
+
+app.listen(port, () => {
+  console.log(`Genius Car Server running on Port ${port}`);
+});
